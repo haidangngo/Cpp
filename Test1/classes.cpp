da8d52dc269a7f1f20da5e477067e5c9e70f69a0
light <haidangngo.e@gmail.com>
2018-11-24 17:37:34 +0700

    1

--- Test1/classes.cpp
+++ Test1/classes.cpp
@@ -1,23 +1,27 @@
 #include <iostream>
 #include <memory>
-class person{
-	protected:
-		static int protectedv;
-	public:
-		void set_val(int val)
-		{
-			protectedv = val;
-		}
-		int get_val()
-		{
-			return protectedv;
-		}
-		virtual void say_hello() = 0;
+class person
+{
+  protected:
+	static int protectedv;
+
+  public:
+	void set_val(int val)
+	{
+		protectedv = val;
+	}
+	int get_val()
+	{
+		return protectedv;
+	}
+	virtual void say_hello() = 0;
 };
 
-class Someone: public person{
-	public:
-	void say_hello() {
+class Someone : public person
+{
+  public:
+	void say_hello()
+	{
 		std::cout << "Hello by friend!" << std::endl;
 	}
 	void say_goodgbye()
@@ -26,18 +30,21 @@ class Someone: public person{
 	}
 };
 
-class Hater: public person{
-	public:
-	void say_hello() {
+class Hater : public person
+{
+  public:
+	void say_hello()
+	{
 		std::cout << "Hello by Hater!" << std::endl;
 	}
 };
 int person::protectedv = 10;
 
-int main() {
+int main()
+{
 	// Check the number of parameters
 	Someone Father;
-	
+
 	//Father.set_val(123);
 	std::cout << Father.get_val() << std::endl;
 	Someone Dang;
@@ -46,10 +53,10 @@ int main() {
 	std::cout << Dang.get_val() << std::endl;
 	Father.set_val(12);
 	std::cout << Father.get_val() << std::endl;
-	std::unique_ptr<person> p (new Hater());
-	
+	std::unique_ptr<person> p(new Hater());
+
 	p->say_hello();
 	std::cout << p->get_val() << std::endl;
-	
-    return 0;
+
+	return 0;
 }
