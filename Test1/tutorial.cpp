f4181041829c800414244b1b022f116cc03d62fc
light <haidangngo.e@gmail.com>
2019-01-09 23:46:42 +0700

    nothing

--- Test1/tutorial.cpp
+++ Test1/tutorial.cpp
@@ -1,30 +1,60 @@
-#include <sstream>
+#include <cmath>
+#include <cstdio>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <sstream>
 using namespace std;
 
-vector<int> parseInts(string str) {
-	// Complete this function
-    stringstream ss(str);
-    int num =0;
-    char ch;
-    vector<int> v;
-    while (!ss.eof()) {
-        ss >> num >> ch;
-        v.push_back(num);
-        cout << num;
+class HRML
+{
+  public:
+    explicit HRML(int n);
+
+  private:
+    vector<string> content;
+    int m_N;
+};
+
+HRML::HRML(int n) : m_N(n)
+{
+    string str;
+    for (int i = 0; i < n; i++)
+    {
+        getline(cin, str);
+        if (str.length() != 0)
+            this->content.push_back(str);
     }
-    return v
 }
 
-int main() {
+class QUERY 
+{
+ public:
+    explicit QUERY(int q);
+
+  private:
+    vector<string> query;
+    int m_Q;
+};
+
+QUERY::QUERY(int q) : m_Q(q)
+{
     string str;
-    cin >> str;
-    vector<int> integers = parseInts(str);
-    for(int i = 0; i < integers.size(); i++) {
-        cout << integers[i] << "\n";
+    for (int i = 0; i < q; i++)
+    {
+        getline(cin, str);
+        if (str.length() != 0)
+            this->query.push_back(str);
     }
-    
+}
+int main()
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, q;
+
+    cin >> n >> q;
+    cin.ignore();
+    HRML hrml(n);
+    QUERY queries(q);
     return 0;
 }
-
