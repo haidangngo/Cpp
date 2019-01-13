ba4182c8ee57d28c005ffe1bb694362a165926ea
U-HAIDANG\MSI <haidanagngo.e@gmail.com>
2019-01-13 14:19:40 +0700

    add tag parser sample code

--- Test1/tutorial.cpp
+++ Test1/tutorial.cpp
@@ -1,60 +1,61 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <sstream>
+
+#include <bits/stdc++.h>
 using namespace std;
 
-class HRML
-{
-  public:
-    explicit HRML(int n);
-
-  private:
-    vector<string> content;
-    int m_N;
-};
-
-HRML::HRML(int n) : m_N(n)
-{
-    string str;
-    for (int i = 0; i < n; i++)
-    {
-        getline(cin, str);
-        if (str.length() != 0)
-            this->content.push_back(str);
-    }
-}
-
-class QUERY 
-{
- public:
-    explicit QUERY(int q);
-
-  private:
-    vector<string> query;
-    int m_Q;
-};
-
-QUERY::QUERY(int q) : m_Q(q)
-{
-    string str;
-    for (int i = 0; i < q; i++)
-    {
-        getline(cin, str);
-        if (str.length() != 0)
-            this->query.push_back(str);
-    }
-}
-int main()
-{
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    int n, q;
-
+int main() {
+    int n,q;
     cin >> n >> q;
     cin.ignore();
-    HRML hrml(n);
-    QUERY queries(q);
-    return 0;
+
+    map <string,string> attributeDB; // to store attribute-value pairs
+    string inputstr,tag_preamble="";
+
+    // get each HRML line
+    for (int i=0;i<n;i++) {
+        getline(cin,inputstr);    
+
+        // for each HRML line, break it up into token words
+        stringstream ss(inputstr);
+        string word, attribute, value;
+        size_t pos;
+        while (getline(ss, word, ' ')) { // for each token word
+            // tag detected -> adjust tag_preamble by +/- tag
+            if (word[0]=='<') {
+                string tag;
+                if (word[1]=='/') { // it's tag closing
+                    tag=word.substr(2);   
+                    tag=tag.substr(0,tag.length()-1); // rid of ">"
+                    pos=tag_preamble.find("."+tag);
+                    if (pos==string::npos) tag_preamble="";
+                    else tag_preamble=tag_preamble.substr(0,pos);
+                }
+                else { // it's tag opening
+                    tag=word.substr(1);
+                    if (tag.find(">")!=string::npos) 
+                    tag=tag.substr(0,tag.length()-1); // rid of ">"
+                    if (tag_preamble=="") tag_preamble=tag;
+                    else tag_preamble=tag_preamble+"."+tag;
+                }
+            }
+            // value detected
+            else if (word[0]=='"') {
+                pos=word.find_last_of('"');
+                value=word.substr(1,pos-1);
+                attributeDB[attribute]=value; // insert into DB
+            }
+            // attribute name detected
+            else if (word[0]!='=') {
+                attribute=tag_preamble + "~" + word;  
+            }
+        }
+    }
+
+    // now we process the queries
+    for (int i=0;i<q;i++) {
+        getline(cin,inputstr); 
+        if (attributeDB.find(inputstr)==attributeDB.end())
+            cout << "Not Found!" << endl;
+        else
+            cout << attributeDB[inputstr] << endl;
+    }
 }
